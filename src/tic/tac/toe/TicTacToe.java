511200638c62f30b5c7bbc31c66c91abe86d4c40
Fatma Hassan <hfatma791@gmail.com>
2022-12-28 00:55:58 +0200

    Update SplashTime

--- src/tic/tac/toe/TicTacToe.java
+++ src/tic/tac/toe/TicTacToe.java
@@ -1,15 +1,48 @@
 package tic.tac.toe;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 public class TicTacToe extends Application {
     @Override
     public void start(Stage primaryStage) {
-        Scene scene = new Scene(new GamesRecordScreenBase(), 750, 480);
+        Parent root = new SplashScreenBase();
+        Scene scene = new Scene(root, 750, 480);
         
+        primaryStage.setTitle("Tic Tac Toe");
         primaryStage.setScene(scene);
         primaryStage.show();
+       
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(TicTacToe.class.getName()).log(Level.SEVERE, null, ex);
+                }
+           
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+
+//                        Parent root = new MainScreenBase();
+//                        Scene scene = new Scene(root, 750, 480);
+//
+//                        primaryStage.setScene(scene);
+//                        primaryStage.show();  
+                        
+                       
+                        
+                    }
+                });
+                 
+            }
+        }).start();
     }
     public static void main(String[] args) {
         launch(args);
