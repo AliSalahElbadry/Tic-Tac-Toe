c397520fb2bd30ccb21e41321697add41b652332
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-01-06 19:57:26 +0200

    PlayerConnection Update

--- src/tic/tac/toe/PlayerConnection.java
+++ src/tic/tac/toe/PlayerConnection.java
@@ -1,6 +1,43 @@
 
 package tic.tac.toe;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class PlayerConnection extends Thread{
+  public DataInputStream recive;
+    public DataOutputStream send;
+
+    Socket socket;
+    String message;
     
+    public PlayerConnection(Socket s){
+        try{
+            message="";
+            socket=s;
+            recive=new DataInputStream(socket.getInputStream());
+            send=new DataOutputStream(socket.getOutputStream());
+            start();
+        }catch(Exception e)
+        {
+            System.out.println(e.getCause());
+        }
+    }
+    @Override
+    public void run() {
+        super.run(); 
+        while(true){
+            try {
+                if(recive!=null){
+                    message=recive.readLine();//here message recived
+                    System.err.println(message);
+                }
+            } catch (Exception ex) {
+                  this.stop();//here set status offline
+                System.out.print(ex.getMessage());
+            }
+        }
+    }
+       
 }
